7a65815878eeaf7184bd1ac254d73583d6b43886
pauloosouzah <pauloosouzah@gmail.com>
2022-05-03 22:45:36 -0300

    .

---
+++ Rw/src/test/java/jp/WebScraping/Rw/RwApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.WebScraping.Rw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
